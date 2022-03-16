1bcdbf16ce2a02dd744d60ba6ccf17a0cc4c0d3a
Lucas de Castro <lucascastrofr@gmail.com>
2022-03-16 11:05:59 -0300

    fix - changing starting port

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,6 @@ const server = new ApolloServer({
   }
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: 5000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
